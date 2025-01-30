19081ad9e639e5c9748c6e3fa545de49b8e634fe
rubn <rucko24@gmail.com>
2025-01-30 14:42:48 +0100

    WIP

--- stream-rabbit-dlq/src/main/java/oz/stream/MessageConsumerConfig.java
+++ stream-rabbit-dlq/src/main/java/oz/stream/MessageConsumerConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.binder.ConsumerProperties;
 import org.springframework.cloud.stream.config.ListenerContainerCustomizer;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
@@ -22,4 +23,5 @@ public class MessageConsumerConfig {
             log.info("Mensaje recibido: {} thread: {}", message, Thread.currentThread().getName());
         };
     }
+
 }

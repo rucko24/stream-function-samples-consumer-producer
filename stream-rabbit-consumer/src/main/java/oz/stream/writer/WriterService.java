ad7c31d0b69bf03d70570b313e082fe2f599cf47
rubn <rucko24@gmail.com>
2025-02-13 00:42:48 +0100

    WIP Added StampedLock on consumer
    * One Thread per Producer

--- stream-rabbit-consumer/src/main/java/oz/stream/writer/WriterService.java
+++ stream-rabbit-consumer/src/main/java/oz/stream/writer/WriterService.java
@@ -1,9 +1,11 @@
 package oz.stream.writer;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PreDestroy;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,13 +13,16 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.locks.StampedLock;
 
+@Log4j2
 @Service
 public class WriterService {
 
     private static final DateTimeFormatter FORMATER = DateTimeFormatter.ofPattern("HH:mm");
+    private static final DateTimeFormatter FORMATER_FILE_NAME = DateTimeFormatter.ofPattern("HH_mm_ss_SSSSS");
     private static final Path OUTPUT = Path.of("/home/rubn/logs/logs.txt");
-
+    private static final StampedLock STAMPED_LOCK = new StampedLock();
 
     @Bean
     public CommandLineRunner createFile() {
@@ -35,6 +40,8 @@ public class WriterService {
 
     public void writer(long latency) {
 
+        var stamped = STAMPED_LOCK.writeLock();
+
         try (final BufferedWriter writer = Files.newBufferedWriter(OUTPUT, StandardOpenOption.APPEND)) {
 
             //HH:mm;latency
@@ -44,7 +51,10 @@ public class WriterService {
 
         } catch (IOException ex) {
             ex.printStackTrace();
+        } finally {
+            STAMPED_LOCK.unlockWrite(stamped);
         }
+
     }
 
 }
